0701dd22fca5e653988654509f520463042eb6cc
DemiGames <lexdelmar@gmail.com>
2022-11-22 19:12:24 +0300

    Module29

---
+++ Assets/Scripts/Touches.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touches : MonoBehaviour
+{
+    public Vector2 touchPosition;
+    Vector2 oldPosition1;
+    Vector2 oldPosition2;
+    void Update()
+    {
+        GetSwipe();
+        MultiTouch();
+    }
+    void GetSwipe()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch;
+            touch = Input.GetTouch(0);
+            if (touch.deltaPosition.x > touchPosition.x && touch.deltaPosition.y < touchPosition.y && touch.deltaPosition.y > -touchPosition.y)
+                Debug.Log("Свайп вправо");
+            //if (touch.deltaPosition.y > touchPosition.y)
+            //    Debug.Log(touch.deltaPosition.y);
+            //if (touch.deltaPosition.y < -touchPosition.y)
+            //    Debug.Log(touch.deltaPosition.y);
+        }
+    }
+    void MultiTouch()
+    {
+        if (Input.touchCount > 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+            Vector2 currentPosition1 = touch.position;
+            Vector2 currentPosition2 = touch2.position;
+            float distance = Vector3.Distance(currentPosition1, currentPosition2);
+            float oldDisstance = Vector3.Distance(oldPosition1, oldPosition2);
+            if (distance > oldDisstance)
+                Debug.Log("Жест увеличение");
+            oldPosition1 = currentPosition1;
+            oldPosition2 = currentPosition2;
+        }
+    }
+}
