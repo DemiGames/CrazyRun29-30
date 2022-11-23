5c6955501de459b71921b5a121dea9b77c0b4303
DemiGames <lexdelmar@gmail.com>
2022-11-23 19:40:03 +0300

    29.1

--- Assets/Scripts/Touches.cs
+++ Assets/Scripts/Touches.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Touches : MonoBehaviour
 {
-    public Vector2 touchPosition;
+    public Vector2 minDistance;
     Vector2 oldPosition1;
     Vector2 oldPosition2;
     void Update()
@@ -18,7 +18,7 @@ public class Touches : MonoBehaviour
         {
             Touch touch;
             touch = Input.GetTouch(0);
-            if (touch.deltaPosition.x > touchPosition.x && touch.deltaPosition.y < touchPosition.y && touch.deltaPosition.y > -touchPosition.y)
+            if (touch.deltaPosition.x > minDistance.x && touch.deltaPosition.y < minDistance.y && touch.deltaPosition.y > -minDistance.y)
                 Debug.Log("Свайп вправо");
             //if (touch.deltaPosition.y > touchPosition.y)
             //    Debug.Log(touch.deltaPosition.y);
@@ -28,12 +28,13 @@ public class Touches : MonoBehaviour
     }
     void MultiTouch()
     {
-        if (Input.touchCount > 1)
+        if (Input.touchCount == 1)
         {
             Touch touch = Input.GetTouch(0);
-            Touch touch2 = Input.GetTouch(1);
+            //Touch touch2 = Input.GetTouch(1);
             Vector2 currentPosition1 = touch.position;
-            Vector2 currentPosition2 = touch2.position;
+            Vector2 currentPosition2 = new Vector2(500, 200);
+            //Vector2 currentPosition2 = touch2.position;
             float distance = Vector3.Distance(currentPosition1, currentPosition2);
             float oldDisstance = Vector3.Distance(oldPosition1, oldPosition2);
             if (distance > oldDisstance)
